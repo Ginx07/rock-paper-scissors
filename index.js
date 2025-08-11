fda9c1141da4170f9fa26666be25b1dcffb78da4
Ginx07 <213358053+Ginx07@users.noreply.github.com>
2025-08-11 14:17:29 -0500

    Created Computers choice

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Testing Testing");
+/*
+CREATE function getComputerChoice 
+RETURN: rock, paper, or scissors using integers 
+CREATE random number generator 
+MAKE getHumanChoice to convert user input of r,p,s into an int 
+INITIALIZE humanScore and computerScore to 0 
+CREATE playRound(humanChoice, computerChoice) 
+MAKE humanChoice case-insensitive 
+INCREMENT score based on round winner 
+SET round limit to 5 in 
+CREATE playGame with playRound inside 
+*/
+
+function getComputerChoice(max){
+    return Math.floor(Math.random() * max);
+
+}
+
+let computerChoice = getComputerChoice(3);
+console.log(computerChoice);
+let choiceName = "";
+
+
+switch(computerChoice){
+    case 0:
+        choiceName = "rock";
+        break;
+    case 1:
+        choiceName = "paper";
+        break;
+    case 3:
+        choiceName = "scissors";
+        break;
+}
+console.log(choiceName);

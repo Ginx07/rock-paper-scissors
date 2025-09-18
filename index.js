69fd293f82f6fb538a9f413ae5a4bfc9190eb740
Ginx07 <213358053+Ginx07@users.noreply.github.com>
2025-09-17 22:29:48 -0500

    Working game with score, and buttons that stop after game

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ SET round limit to 5 in
 CREATE playGame with playRound inside 
 */
 
-
 let playerScore = 0;
 let computerScore = 0;
 let computerChoice = "";
@@ -21,15 +20,17 @@ function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function playRound(humanChoice, computerInt) {
+function playRound(humanChoice) {
     const names = ["rock", "paper", "scissors"]
     const computerChoice = names[getComputerChoice()]
 
     let result = "";
 
+    //running game until 5 wins
     if (humanChoice === computerChoice) {
         result = "This round is a tie."
     }
+    //all of the valid outcomes for player win
     else if (
         humanChoice === "rock" && computerChoice === "scissors" ||
         humanChoice === "paper" && computerChoice === "rock" ||
@@ -42,7 +43,35 @@ function playRound(humanChoice, computerInt) {
         result = "Computer wins this round!";
         computerScore++;
     }
+    if (playerScore === 5 || computerScore === 5) {
+        //end game
+        //reset score
+        document.querySelector("#score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
 
+        //stopping buttons function
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+
+        //setting all values to 0
+        let resetBtn = document.querySelector("#play-again");
+        resetBtn.style.display = "inline";
+        resetBtn.addEventListener("click", () => {
+            playerScore = 0;
+            computerScore = 0;
+            document.querySelector("#results").textContent = "";
+            document.querySelector("#score").textContent = `Player: ${playerScore} | Computer: ${computerScore}`;
+            resetBtn.style.display = "none";
+        
+            rockBtn.disabled = false;
+            paperBtn.disabled = false;
+            scissorsBtn.disabled = false;
+        });
+
+
+    }
+
+    //displaying results on page
     let resultsDiv = document.querySelector("#results")
     resultsDiv.textContent = `You chose ${humanChoice}, computer chose ${computerChoice}.
                             ${result} Score: You ${playerScore} - Computer ${computerScore}`;
@@ -57,7 +86,12 @@ rockBtn.addEventListener("click", () => playRound("rock"));
 paperBtn.addEventListener("click", () => playRound("paper"));
 scissorsBtn.addEventListener("click", () => playRound("scissors"));
 
+/*
+    -- A fix for simplififying this function, would have to alter playround const to work --
+    function getComputerChoice() {
+    //creating random number to use as selector in choice matching text
 
-//adding listeners
-//match playRound
-
+    const names = ["rock", "paper", "scissors"]
+    const computerChoice = names[Math.floor(Math.random() * 3)]
+    return computerChoice;
+} */
